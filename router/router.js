73a540be25bda50ff0da3bc30b5fc4c095947f78
Sarthak30-03-2003 <sarthak8544@gmail.com>
2023-03-04 18:49:27 +0530

    routes

--- router/router.js
+++ router/router.js
@@ -2,8 +2,24 @@ import { Router } from "express";
 
 const router = Router();
 
-router.route('/').get((req,res)=>{
-    res.send("heigeh")
+/**POST Methods */
+router.route('/register').post((req, res) => {
+    res.status(201).json("Register Route")
 })
+router.route('/registerMail').post();
+router.route('/authenticate').post();
+router.route('/login').post();
+
+
+/**GET Methods */
+router.route('users/:username').get();
+router.route('/generateOTP').get();
+router.route('/verifyOTP').get();
+router.route('/createResetSession').get();
+
+/** PUT Methods */
+
+router.route('/updateUser').put();
+router.route('/resetPassword').put();
 
 export default router;
